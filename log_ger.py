aade1e09456d5bfaf9f6c52be5f916a8ba333127
ghanad <48297368+ghanad@users.noreply.github.com>
2019-05-22 23:55:15 +0430

    Add files via upload

--- log_ger.py
+++ log_ger.py
@@ -1,44 +1,48 @@
 #! /usr/bin/env python3
-
-# version 1.0
-# 2/13/2019
-# first edition
-
-##########
-##copy to liberary directory
-##cp log_ger.py /usr/lib/python3/dist-packages/
-
-##########
-##   to use as module:
-#import log_ger
-#my_logger = log_ger.get_logger(__name__ + "Logger")
-#my_logger.debug("---------a debug message")
+#version 1.3
 
 import logging
 import sys
 from logging.handlers import TimedRotatingFileHandler
 
-FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
+FORMATTER = logging.Formatter(
+    "%(asctime)s — %(name)s — %(levelname)s — %(message)s")
 LOG_FILE = "my_app.log"
+
 
 def get_console_handler():
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(FORMATTER)
         return console_handler
 
-def get_file_handler():
-        file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+
+def get_file_handler(log_file):
+        file_handler = TimedRotatingFileHandler(log_file, when='midnight')
         file_handler.setFormatter(FORMATTER)
         return file_handler
 
-def get_logger(logger_name):
+
+def get_logger(logger_name=sys.argv[0][2:-3], console=True, file=False, log_file=sys.argv[0][2:-3]+'_report.txt'):
         logger = logging.getLogger(logger_name)
-        logger.setLevel(logging.DEBUG)                  #Log level
-        logger.addHandler(get_console_handler())        #to print logs on console
-        #logger.addHandler(get_file_handler())          #to write logs on file
+        logger.setLevel(logging.DEBUG)
+
+        # to print logs on console
+        if console == True:
+                logger.addHandler(get_console_handler())
+        
+        # to write logs on file
+        if file == True:
+                logger.addHandler(get_file_handler(log_file))
+
         logger.propagate = False
         return logger
 
 
 #my_logger = get_logger(__name__ + "Logger")
+#my_logger.debug("---------a debug message")
+
+##########
+# to use as module:
+#import log_ger
+#my_logger = log_ger.get_logger(__name__, console=True, file=False)
 #my_logger.debug("---------a debug message")
