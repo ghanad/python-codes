3b2e176adf51d191e5fb5d4db9e429389a77d0f8
ghanad <48297368+ghanad@users.noreply.github.com>
2019-06-28 12:11:31 +0430

    fix password problem

--- parallel-ssh.py
+++ parallel-ssh.py
@@ -4,12 +4,12 @@ from __future__ import print_function
 from pssh.clients import ParallelSSHClient
 
 username = 'root'
-password = 'a'
+pass_word = 'a'
 port = 22
 log_path = '/var/log/pnd-ipos.log'
 
 hosts = ['10.1.1.1', '10.1.1.2']
-client = ParallelSSHClient(hosts, port=port , user=username, password=passwrod)
+client = ParallelSSHClient(hosts, port=port , user=username, password=pass_word)
 
 output = client.run_command('echo -ne "$(cat /etc/hostname) | " ;echo -ne "$(date | cut -d " " -f 2,3,4) | " ;  iostat -x 2 5 | grep sda | tail -n 1')
 
