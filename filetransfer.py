2bae52cced1f6f5d32a235cc2fe680685f1e3e31
ghanad <48297368+ghanad@users.noreply.github.com>
2019-07-05 00:09:41 +0430

    Change logs for errors.

--- filetransfer.py
+++ filetransfer.py
@@ -3,28 +3,26 @@
 import logging
 import paramiko
 
-username = 'root'
-password = 'a'
-
 
 logger = logging.getLogger(__name__)
 
 
-def file_recv(ip , username, password , src_file, dst_file, port):
-	logger.info('{{file_recv}} Start.')
+def file_recv(ip, username, password, src_file, dst_file, port):
+    logger.info('Start file recive function.')
 
-	ssh = paramiko.SSHClient()
-	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-	try:
-		ssh.connect(ip, username=username, password=password, timeout=5 , port=port)
-	except:
-		logger.error('{{file_recv}} Connection failed')
-		return False
-	sftp = ssh.open_sftp()
-	try:
-		sftp.get(src_file, dst_file)
-	except Exception as error:
-		logger.error('{{file_recv}} %s' % error)
-		return False
-	sftp.close()
+    try:
+        ssh.connect(ip, username=username,
+                    password=password, timeout=5, port=port)
+    except Exception as connection_error:
+        logger.error('Connection failed %s' % connection_error)
+        return False
+    sftp = ssh.open_sftp()
+    try:
+        sftp.get(src_file, dst_file)
+    except Exception as error:
+        logger.error('Error in recive file %s' % error)
+        return False
+    sftp.close()
