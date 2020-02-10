80a355c3f65d84ee64ac1c881078bdcd0c183e94
unknown <am.ag65@gmail.com>
2020-02-10 14:10:03 +0330

    complete tokens class

--- cassandra_query.py
+++ cassandra_query.py
@@ -1,29 +1,74 @@
 from configparser import ConfigParser
 from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor 
 import os
 
 
 TOKEN_FILE = 'tokens.ini'
+INI_GROUP_NAME = 'tokens'
 
 class Tokens:
-    def __init__(self):
-        pass
-    # True = continue 
-    # False = create new file 
-    def getUserAnswer(self, tokenFile=TOKEN_FILE):
-        if os.path.isfile(tokenFile):
-            user_answer = input('{} file is available. do you want to continue?[Y/N]'.format(tokenFile))
-            if user_answer == 'Y' or user_answer == 'y':
-                return True
-            else:
-                return False
-        else:
-            return False
-    
-    def touchFile(self, userAnswer):
-        if userAnswer == False:
-            with open(TOKEN_FILE, 'w') as tmpinifile:
-                tmpinifile.write()
+    def __init__(self, tokenNumber, tokenFile=TOKEN_FILE, GroupName=INI_GROUP_NAME):
+        self.tokenFile = tokenFile
+        self.GroupName = GroupName
+        self.tokenNumber = tokenNumber
 
-a = Tokens()
-a.touchFile(a.getUserAnswer())
+    def checkFileExist(self):
+        if os.path.exists(self.tokenFile):
+            user_answer = input('{} file is available. do you want to continue?[Y/N]'.format(self.tokenFile))
+            if user_answer == 'Y' or user_answer == 'y':
+                return self.loadINIfile()
+            else:
+                os.remove(self.tokenFile)
+                for k,v in self.tokenCreator().items():
+                    self.saveToINI(k,v)
+                # print(self.tokenCreator())
+                return self.tokenCreator()
+        else:
+            for k,v in self.tokenCreator().items():
+                    self.saveToINI(k,v)
+            return self.tokenCreator()
+
+    def tokenCreator(self):
+        rangeList = dict()
+        start = -9223372036854775808
+        end = 9223372036854775807
+        startRange = start
+        rangeSize = int((end - start) / self.tokenNumber)
+        for num,i in enumerate(range(0, self.tokenNumber)):
+            endRange = startRange + rangeSize
+            if i == (self.tokenNumber - 1):
+                endRange = end
+            # rangeList.append('{},{},{}'.format(startRange, endRange,num))
+            rangeList['{},{},{}'.format(num, startRange, endRange)] = 'None'
+            startRange = endRange  
+        return rangeList
+
+    def loadINIfile(self):
+        config = ConfigParser()
+        config.read(self.tokenFile)
+        return {k:v for k,v in config.items(self.GroupName)}
+
+    def saveToINI(self, token, status):
+        config = ConfigParser()
+        config.read(self.tokenFile)
+        if not config.has_section(self.GroupName):
+            config.add_section(self.GroupName)
+        config.set(self.GroupName, token, status)
+        with open(self.tokenFile, 'w') as configfile:
+            config.write(configfile)
+
+# a = Tokens(100)
+# tokensDict = a.checkFileExist()
+
+def testfunction(a):
+    print('{}   {}'.format(os.getpid(), a))
+
+
+
+if __name__ == "__main__":
+    a = Tokens(100)
+    tokensDict = a.checkFileExist()
+    noneTokenList = [x for x in tokensDict.keys()  if tokensDict[x] != 'Success']
+    with ProcessPoolExecutor(max_workers=10) as executor:
+        executor.map(testfunction, noneTokenList)
