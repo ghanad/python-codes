3fb52fd201799375e1e5bf84cc10c70b0c3fa990
ghanad <48297368+ghanad@users.noreply.github.com>
2020-02-16 21:44:22 +0330

    remove empty lines

--- digikal.py
+++ digikal.py
@@ -11,6 +11,7 @@ def getPrice(urlCode):
     req = urllib.request.Request(source_url, None, {'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'})
     urlSource = urllib.request.urlopen(req).read()
     soup = bs.BeautifulSoup(urlSource, 'lxml')
+    # print(soup.title.string)
     sellPrice = int(soup.find_all("div", class_ = 'c-product__seller-price-raw js-price-value')[0].string.strip().replace(',',''))
     try:
         originalPrice = int(soup.find_all("div", class_ = 'c-product__seller-price-prev js-rrp-price')[0].string.strip().replace(',',''))
